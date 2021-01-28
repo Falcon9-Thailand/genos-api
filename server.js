d40473a6f5d4517fd13c50f9452414a9a43822d1
LUFFY <denk2eror@gmail.com>
2021-01-28 13:41:03 +0700

    test5

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 require("dotenv").config();
+const { Client } = require('pg');
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = process.env.PORT || process.env.APP_PORT;
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+  
+  client.connect();
+  
+  client.query('SELECT * FROM test1;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  });
+
 app.use(bodyParser());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(cors());
+
 
 app.get('/',(req,res)=>{
     res.send('hello');
