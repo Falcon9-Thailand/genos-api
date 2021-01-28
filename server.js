f68eb1af533cf279e9ebdbfea423f748c09acfb1
LUFFY <denk2eror@gmail.com>
2021-01-28 09:40:32 +0700

    get sql2

--- server.js
+++ server.js
@@ -4,23 +4,25 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const pool = require('./config/connectdb');
-const port = process.env.PORT || process.env.APP_PORT;
 app.use(bodyParser());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {   
-    pool.query('SELECT * FROM test1',[],(error,result,fields)=>{
+app.get('/', (req, res) => {  
+    try {
+          pool.query('SELECT * FROM test1',[],(error,result,fields)=>{
         if(error){
             res.statusCode(400).send({error:"errors"});
         }
         res.send({success:result});
     });
-    
+    } catch (error) {
+        res.send({success:error});
+    }   
  })
 
 
-app.listen(port, () => {
-    console.log("server running port:", process.env.APP_PORT) // แสดงผล บน Console APP_PORT at 3000
+app.listen(process.env.DB_PORT, () => {
+    console.log("server running port:", process.env.DB_PORT) // แสดงผล บน Console APP_PORT at 3000
 });
