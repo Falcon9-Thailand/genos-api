97cdfe7d7ed7bd93b79d8e28070a73943edc3a16
LUFFY <denk2eror@gmail.com>
2021-01-28 13:56:57 +0700

    create table

--- server.js
+++ server.js
@@ -13,23 +13,22 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(cors());
 
-
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false
     }
   });
-  
   client.connect();
   
-  client.query('SELECT * FROM Persons;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  });
+
+  app.get('/',(req,res)=>{
+    client.query('SELECT * FROM Persons;', (err, result) => {
+        if (err) throw err;
+        res.send({data:result});
+        client.end();
+      });
+  })
 
 app.listen(port, () => {
     console.log("server running port:", port) // แสดงผล บน Console APP_PORT at 3000
