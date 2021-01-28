4ab0f05ea3d770c2f24ddb36020da72686ba26e9
LUFFY <denk2eror@gmail.com>
2021-01-28 09:48:00 +0700

    get sql5

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const pool = require('./config/connectdb');
+const port = process.env.PORT || process.env.APP_PORT;
 app.use(bodyParser());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {  
-    res.send({success:"true"}) ;
+app.get('/', (req, res) => {    
+    res.send({success:"first app"})
  })
 
 
-app.listen(process.env.PORT, () => {
-    console.log("server running port:", process.env.DB_PORT) // แสดงผล บน Console APP_PORT at 3000
+app.listen(port, () => {
+    console.log("server running port:", process.env.APP_PORT) // แสดงผล บน Console APP_PORT at 3000
 });
