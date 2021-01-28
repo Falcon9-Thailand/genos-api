6c24faf0f43bd1c982c19f1ea659589a95fc4821
LUFFY <denk2eror@gmail.com>
2021-01-28 09:32:54 +0700

    get sql

--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const pool = require('./config/connectdb');
 const port = process.env.PORT || process.env.APP_PORT;
 app.use(bodyParser());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {    
-    res.send({success:"first app"})
+app.get('/', (req, res) => {   
+    pool.query('SELECT * FROM test1',[],(error,result,fields)=>{
+        if(error){
+            res.statusCode(400).send({error:"errors"});
+        }
+        res.send({success:result});
+    });
+    
  })
 
 
